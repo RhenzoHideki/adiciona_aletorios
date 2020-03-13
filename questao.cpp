aa37bb80315caf2027b36ba8c149661c2b628f74
RhenzoHideki <rhskgames@gmail.com>
2020-03-13 10:39:32 -0300

    commit

--- questao.cpp
+++ questao.cpp
@@ -1,1 +1,15 @@
 // Implemente aqui a função
+
+#include "questao.h"
+
+void adiciona_aleatorios(list<int> & umaLista, int N){
+
+    srand(time(NULL)); //começa o contador para o aleatório
+
+    for (int i = 0; i < N ; ++i)
+    {
+        umaLista.push_back(rand()%70);
+    }
+
+    umaLista.sort();
+}
--- questao.h
+++ questao.h
@@ -7,6 +7,13 @@
 
 // declare aqui a função
 // não esqueça dos include !
+#include "questao.h"
+#include <list>
+#include <iostream>
+using namespace std;
+
+
+void adiciona_aleatorios(list<int> & umaLista, int N);
 
 
 #endif //PROG_QUESTAO_H
